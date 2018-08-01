bb190f9eee599e07503fcdf9cd36382da983d6dd
verdantpath <m_dittmann@yahoo.com>
2018-07-31 22:46:49 -0700

    add scripts.js

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const assignmentTitleField = document.getElementById('assignment-title');
+const pointsPossibleField = document.getElementById('points-possible');
+const pointsReceivedField = document.getElementById('points-received');
+const btnAddEntry = document.getElementById('submit');
+
+const addEntry = (e) => {
+  e.preventDefault;
+  console.log('function addEntry called');
+  var gradeEntry = {
+    assignmentTitle: assignmentTitleField.value,
+    pointsPossible: pointsPossibleField.value,
+    pointsReceived: pointsReceivedField.value,
+    gradePercentage: function() {
+      return  gradeEntry.pointsReceived / gradeEntry.pointsPossible;
+    }
+  };
+  console.log(gradeEntry.assignmentTitle);
+  console.log(gradeEntry.pointsPossible);
+  console.log(gradeEntry.pointsReceived);
+  console.log(gradeEntry.gradePercentage());
+}
+
+btnAddEntry.addEventListener('click', addEntry);
