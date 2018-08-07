7631f8a614fa0fdba6c49b570ee749f3b1b989e0
verdantpath <m_dittmann@yahoo.com>
2018-08-06 21:32:42 -0700

    update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ const assignmentTitleField = document.getElementById('assignment-title');
 const pointsPossibleField = document.getElementById('points-possible');
 const pointsReceivedField = document.getElementById('points-received');
 const btnAddEntry = document.getElementById('submit');
+const outputContainer = document.getElementById('grade-table');
+
+var output = '<div class="assignment-container"></div>';
 
 const addEntry = (event) => {
   event.preventDefault();
@@ -18,6 +21,8 @@ const addEntry = (event) => {
   console.log(gradeEntry.pointsPossible);
   console.log(gradeEntry.pointsReceived);
   console.log(gradeEntry.gradePercentage());
+  var output = '<div class="assignment-container"></div>';
+  outputContainer.innerHTML = output;
 }
 
 btnAddEntry.addEventListener('click', addEntry);
